96cc9e6c1b3cae6bb84fc73fe44d8e33a348495b
songshuai <songshuai@aiyoutoo.com>
2022-04-29 08:42:15 +0800

    添加 三弟跳舞例子

---
+++ src/components/MyWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Box from "./Box.vue"
+import Floor from "./Floor.vue"
+const lock = ref(false);
+</script>
+
+<template>
+    <SDWebglRenderer @click="() => { lock = true }" :width="720" :height="360" :backgroundColor="0x1f63d1">
+        <SDPointerLockControls :lock="lock" :unlockCallback="() => { lock = false }"  :options="{ junpHeight: 10 }"/>
+        <SDScene>
+            <SDPerspectiveCamera :positionZ="2" :positionY="0.5" />
+            <Floor :positionX="-5" :positionZ="-5" />
+            <Box :positionX="-5" :positionZ="-5" />
+        </SDScene>
+    </SDWebglRenderer>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Sandi.vue
@@ -0,0 +1,18 @@
+<template>
+    <SDWebglRenderer :width="800" :height="400">
+        <SDScene>
+            <SDPerspectiveCamera :positionZ="20" :positionY="6" />
+            <SDFBXLoader url="/fbx/Rumba Dancing.fbx">
+                <SDMeshBasicMaterial meshName="body1">
+                    <SDTextureLoader url="img/zhangfei.jpg" type="map" />
+                </SDMeshBasicMaterial>
+                <SDMeshBasicMaterial meshName="face">
+                    <SDTextureLoader url="img/face.png" type="map" />
+                </SDMeshBasicMaterial>
+                <SDAnimationMixer>
+                    <SDAnimationAction />
+                </SDAnimationMixer>
+            </SDFBXLoader>
+        </SDScene>
+    </SDWebglRenderer>
+</template>
